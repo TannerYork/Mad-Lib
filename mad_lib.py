8c611963e27e06ce1ed1f3b22c6104b9a7a062eb
Tanner York <tannerwyork@gmail.com>
2019-08-28 22:15:40 -0700

    Make create_mad_lib function accept a single input instead of multiple

--- mad_lib.py
+++ mad_lib.py
@@ -1,7 +1,7 @@
-def create_mad_lib(adjective, noun, verb, indoor_activity, person, outdoor_activity):
-    return f""" Kevin is a very {adjective} {noun}, he gets really {verb} when people have the same name as him. 
- Sometimes, Kevin enjoys {indoor_activity} inside with his friend {person}. When Kevin and {person} 
- can't {indoor_activity} inside they choose to {outdoor_activity} outside instead. """
+def create_mad_lib(input):
+    return f""" Kevin is a very {input["adjective"]} {input["noun"]}, he gets really {input["verb"]} when people have the same name as him. 
+ Sometimes, Kevin enjoys {input["indoor_activity"]} inside with his friend {input["person"]}. When Kevin and {input["person"]} 
+ can't {input["indoor_activity"]} inside they choose to {input["outdoor_activity"]} outside instead. """
 
 print(""" Kevin is a very {adjective} {noun}, he gets really {verb} when people have the same name as him. 
  Sometimes, Kevin enjoys {indoor activity} inside with his friend {person}. When Kevin and {person} can't 
@@ -14,4 +14,13 @@ indoor_activity = str(input("indoor activity: "))
 person = str(input("person: "))
 outdoor_activity = str(input("outdoor activity: "))
 
-print(create_mad_lib(adjective, noun, verb, indoor_activity, person, outdoor_activity))
+input = {
+    "adjective": adjective,
+    "noun": noun,
+    "verb": verb,
+    "indoor_activity": indoor_activity,
+    "person": person,
+    "outdoor_activity": outdoor_activity
+}
+
+print(create_mad_lib(input))
