48a4f9003eca85a4f9ce55d501544ef3288d5d59
Tanner York <tannerwyork@gmail.com>
2019-08-31 18:35:41 -0700

    Reformat To Work With Regular Expressions

--- madlibs.py
+++ madlibs.py
@@ -1,67 +1,34 @@
 import random
+import re
 
 class Mad_Lib:
     def __init__(self, mad_lib):
-        self.user_input = {
-            "name": None,
-            "nouns": [],
-            "verbs": [],
-            "adjectives": [],
-            "adverbs": []
-        }
         self.mad_lib = mad_lib
+        
+    # Take any text inclosed in [] and inserts it into the inputs array  
+    def user_inputs(self):
+        inputs = re.findall('\[([a-z]([a-z]|\s)+)\]', self.mad_lib)
+        for index, item in enumerate(inputs): inputs[index] = item[0]
+        user_inputs = {}
+        for item in inputs: user_inputs[item] = input(f"{item}: ")
+        return user_inputs
 
-    def get_input(self, input_type):
-        for word in self.mad_lib:
-            if(word == input_type):
-                inputs = [input(f'{input_type}: ')]
-                while(inputs[0] is ''):
-                    inputs[0] = input(f'Invaled, {input_type} is required: ')
-                self.user_input[f"{input_type}s"].append(inputs[0])
-    
-    def get_name_input(self):
-        name = input('Name: ')
-        while(name is ''):
-            name = input('Invaled, a name is required: ')
-    
-    def get_user_input(self):
-        self.get_name_input()
-        self.get_input('noun')
-        self.get_input('verb')
-        self.get_input('adjective')
-        self.get_input('adverb')
-
-    def insert_input(self, list_type):
-        for i, word in enumerate(self.mad_lib):
-            if(word == list_type):
-                random_word = random.choice(self.user_input[f"{list_type}s"])
-                self.mad_lib[i] = random_word
-                self.user_input[f"{list_type}s"].remove(random_word)
-
-    def insert_name_input(self):
-        for word in self.mad_lib:
-            if(word == 'name'):
-                word = self.user_input["name"]
-
+    # Subbsitute bracket inclosed text with user input
     def create(self):
-        self.insert_name_input()
-        self.insert_input('noun')
-        self.insert_input('verb')
-        self.insert_input('adjective')
-        self.insert_input('adverb')
+        for key, value in self.user_inputs().items():
+            self.mad_lib = re.sub(f'\[{key}\]', value, self.mad_lib)
 
-    def init(self):
-        self.get_user_input()
-        self.create()
-        print("".join(self.mad_lib))
-        
-        
-        
-jack_and_bean = ['Once upon a time there lived a ', 'adjective', ' ', 'noun', ' and her ', 'noun',' ', 'name', '. One day, ', 'name', "'s ", 'noun', ' told him to ', 'verb', ' their only ', 'noun', '.', ' name', ' went to the ', 'noun', ' and on the way he met a man who wanted to buy his ', 'noun', '. Jack asked, ', '"What will you give me for my ', 'noun', '? The man answered, “I will ', 'verb', ' you five ', 'adjective', ' ', 'noun', '!” ', 'name', ' took the ', 'adverb',' ', 'verb',' ', 'noun', ' and gave the man the ', 'noun', '. But when he', 'verb', ' ', 'place', ' ', 'name', "'s ", 'noun', ' was very ', 'verb', '. She said, “You ', 'adjective', '! He took away your ', 'noun', ' and gave you some ', 'noun', '!” She ', 'verb', ' the ', 'noun', ' out of the ', 'noun', '. ', 'name', ' was very ', 'adjective', ' and went to ', 'verb', ' without ', 'noun', '.']
-kevin = ['Kevin is a very ', 'adjective', ' ', 'noun', ' he gets really ', 'verb', ' when people have the same name as him. Sometimes, Kevin enjoys ', 'verb', ' with his friend ', 'noun', '.']
+    def print(self):
+        print(self.mad_lib)
 
-kevin_mad_lib = Mad_Lib(kevin)
-kevin_mad_lib.init()
+global_mad_lib = '''
+        Kevin is a very [adjective] [noun], he gets really [verb] people have 
+        the same name as him. Sometimes, Kevin enjoys [outdoor activity] with 
+        his friend [noun].
+        '''
+mad_lib = Mad_Lib(global_mad_lib)
+mad_lib.create()
+mad_lib.print()
 
 def test():
      print(f'''
